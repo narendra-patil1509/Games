f55dfcc402346516b569c2bb9298284775b3378c
narendra-patil1509 <np738587@gmail.com>
2024-01-05 17:04:22 +0530

    playermove function added

--- 02-Saap-Sidi/script.js
+++ 02-Saap-Sidi/script.js
@@ -1,9 +1,34 @@
 let generatedNumber;
 let sum = 0;
+let previousNumber = 1;
+let currentNumber = 0;
 let dice = document.getElementById("sum");
+let player = document.getElementById("player");
+let circle = document.getElementById("circle");
+
 function rollDice(){
     generatedNumber = Math.floor((Math.random()*6)+1);
     dice.innerText = generatedNumber;
     sum+=generatedNumber;
-    console.log("Sum = ",sum);
+    currentNumber = sum;
+
+    for (let i = previousNumber; i <= currentNumber; i++) {
+        let boxValue = document.getElementById("box"+ i);
+        const rect = boxValue.getBoundingClientRect();
+        const position = {
+            left: rect.left,
+            top: rect.top,
+        };
+
+        let top = position.top;
+        let left = position.left;
+
+        playerMove(top,left);
+    }
+    // console.log("Sum = ",sum);
+}
+
+function playerMove(top,left){
+    console.log("PLayer Move");
+    player.style.transform = `translateY(${top-189}px) translateX(${left-21}px)`;
 }
