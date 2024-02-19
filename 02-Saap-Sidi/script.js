9a6a4ce150ab6be3225ef64a6083fb1609cb274e
narendra-patil1509 <np738587@gmail.com>
2024-02-19 17:15:50 +0530

    twice time play when roll diced and get 6

--- 02-Saap-Sidi/script.js
+++ 02-Saap-Sidi/script.js
@@ -1,9 +1,5 @@
 let generatedNumber;
-// let sum = 0;
-// let previousNumber = 1;
-// let currentNumber = 0;
 let dice = document.getElementById("sum");
-
 let button = document.getElementById("btn");
 let circle = document.getElementById("circle");
 let diceImage = document.getElementById("dImage");
@@ -20,13 +16,25 @@ class Player{
         this.currentNumbers = 0;
         this.generatedNumbers = 1;
     }
-
     playerSum(generatedNumbers){
+        this.getCurrentNum();
         this.sums+= generatedNumbers;
-        this.currentNumbers = this.sums;
+        this.currentNumbers = this.getCurrentNum()+ generatedNumber;
         this.previousNumbers = this.currentNumbers - this.previousNumbers;
         console.log("Player Current Sum = ",this.currentNumbers);
         return this;
+    }
+    setCurrentNum(cu){
+        this.currentNumbers = cu;                                       
+    }
+    setsumsNum(sums){
+        this.sums;                                       
+    }
+    getCurrentNum(){
+       return this.currentNumbers;                                       
+    }
+    getsumsNum(){
+       return this.sums;                                       
     }
 }
 
@@ -37,7 +45,6 @@ let ps4 = new Player();
 
 function whichplayer(){
     if(o==5){
-        console.log("Op = ",o);
         o=1;
     }
     sessionStorage.setItem("Player-Number",o);
@@ -46,100 +53,95 @@ function whichplayer(){
 }
 
 function rollDice(){
-    let il = parseInt(sessionStorage.getItem("Player-Number"));
-    console.log(il);
-    
-    p=il;
+    let p = parseInt(sessionStorage.getItem("Player-Number")); 
     generatedNumber = Math.floor((Math.random()*6)+1);
-
-    if(il == 1){
+    if(generatedNumber == 6){
+        o = p;
+        // alert("You rolled a six! It's your turn again!",o);
+    }
+    if(p == 1){
+        button.style.background = "#2C7408";
+        button.style.color = "#fff";
         let ob1 = ps1.playerSum(generatedNumber);
         console.log("Player 1  Generated Num = ",generatedNumber);
-        positionIteration(il,ob1.currentNumbers);
+        positionIteration(p,ob1);
     }
-    if(il == 2){
+    if(p == 2){
+        button.style.background = "#00DDFF";
+        button.style.color = "#000";
         let ob2 = ps2.playerSum(generatedNumber);
         console.log("Player 2 Generated Num = ",generatedNumber);
-        positionIteration(il,ob2.currentNumbers);
+        positionIteration(p,ob2);
     }
-    if(il == 3){
+    if(p == 3){
+        button.style.background = "#9B10C5";
+        button.style.color = "#fff";
         let ob3 = ps3.playerSum(generatedNumber);
         console.log("Player 3 Generated Num = ",generatedNumber);
-        positionIteration(il,ob3.currentNumbers);
+        positionIteration(p,ob3);
     }
-    if(il == 4){
+    if(p == 4){
+        button.style.background = "#DBF300";
+        button.style.color = "#000";
         let ob4 = ps4.playerSum(generatedNumber);
         console.log("Player 4 Generated Num = ",generatedNumber);
-        positionIteration(il,ob4.currentNumbers);
+        positionIteration(p,ob4);
     }
+    console.log("----------------------------------");
     diceImage.src = `images/Untitled design/${generatedNumber}.png`;
     diceImage.style.transform = `rotate(${2*generatedNumber*180}deg)`;
     diceSound.play();
-    // dice.textContent = generatedNumber;
-    sum+=generatedNumber;
-    currentNumber = sum;
+    // if(sum>=100){
+    //     console.log(sum,"sum");
+    //     button.removeAttribute("onclick");
+    //     button.style.display = 'none';
+    //     //Winneer Window will show here
+    // }
 
-    if(sum>=100){
-        console.log(sum,"sum");
-        button.removeAttribute("onclick");
-        button.style.display = 'none';
-
-        //Winneer Window will show here
-        
-    }
-
-    function positionIteration(p,cns){
+    function positionIteration(p,ob){
         let pns = 1;
+        let cns = ob.currentNumbers;
         for (let i = pns; i <= cns; i++) {
             let boxValue = document.getElementById("box"+ i);
             let t = boxValue.offsetTop;
             let l = boxValue.offsetLeft;
-            playerMove(t,l,p);
+            
+            if(cns == 5){
+                //Sidi functionality 5 to 25 only(Jump from 5 to 26)
+                boxValue = document.getElementById("box26");
+                let tt = boxValue.offsetTop;
+                let ll = boxValue.offsetLeft;
+                ob.setCurrentNum(26);
+                playerMove(t,l,p);
+                setTimeout( ()=>{
+                    saapSidi(tt,ll,p);
+                },1500)
+            }
+            else if(cns == 31){
+                //Saap functionality 34 to 4 only(Jump from 34 to 4)
+                sum = 8;
+                boxValue = document.getElementById("box8");
+                let tt = boxValue.offsetTop;
+                let ll = boxValue.offsetLeft;
+                ob.setCurrentNum(4);
+                playerMove(t,l,p);
+                setTimeout( ()=>{
+                    saapSidi(tt,ll,p);
+                },1500);
+            }
+            else{
+                playerMove(t,l,p);
+            }            
         }
     }
-    // for (let i = previousNumber; i <= currentNumber; i++) {
-    //     console.log("i = ",i);
-    //     let boxValue = document.getElementById("box"+ i);
-    //     let t = boxValue.offsetTop;
-    //     let l = boxValue.offsetLeft;
-
-    //     if(currentNumber == 5){
-    //         //Sidi functionality 5 to 25 only(Jump from 5 to 25)
-    //         sum = 26;
-    //         boxValue = document.getElementById("box26");
-    //         let tt = boxValue.offsetTop;
-    //         let ll = boxValue.offsetLeft;
-    //         playerMove(t,l,p);
-    //         setTimeout( ()=>{
-    //             saapSidi(tt,ll,p);
-                
-    //         },1500)
-    //     }
-    //     else if(currentNumber == 31){
-    //         //Saap functionality 34 to 4 only(Jump from 34 to 4)
-    //         sum = 8;
-    //         boxValue = document.getElementById("box8");
-    //         let tt = boxValue.offsetTop;
-    //         let ll = boxValue.offsetLeft;
-    //         playerMove(t,l,p);
-    //         setTimeout( ()=>{
-    //             saapSidi(tt,ll,p);
-    //         },1500)
-            
-    //     }
-    //     else{
-    //         playerMove(t,l,p);
-    //     }
-    // }
 }
 function saapSidi(tt,ll,p){
     let player = document.getElementById("player"+p);
-    player.style.top = tt+7+"px"; 
-    player.style.left = ll-4+"px";
+    player.style.top = tt+7+6+"px";
+    player.style.left = ll-4+16+"px";
 }
 
 function playerMove(t,l,p){
-    // console.log("PlayerMove = ",p);
     let player = document.getElementById("player"+p);
     player.style.top = t+12+"px";
     player.style.left = l+13+"px";
@@ -150,4 +152,4 @@ window.onload = ()=>{
         p=1;
         console.log("Onload P = ",p);
     }
-  }
+}
